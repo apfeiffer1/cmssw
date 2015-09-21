aac6ea37aab0ca7a954784bcb6c0614f092fe95c
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2015-09-21 11:37:20 +0200

    move core of simple http client to lib, adapt test accordingly. add use-ing of cpp-netlib to main buildfile

---
+++ CondCore/Utilities/src/CondSimpleHTTPClient.h
@@ -0,0 +1,10 @@
+
+#include <boost/network/protocol/http/client.hpp>
+#include <boost/network/uri.hpp>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+namespace cond {
+  int simpleHTTPClient( std::string url );
+}
